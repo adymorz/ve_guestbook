c9519c0a87209c7c89807e1fae5751d4feb7b614
Adrian Dymorz <dev@adrian.dymorz.ch>
2020-08-08 20:17:08 +0200

    Mark as compatible with T3 9 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0 - 8.9.99',
+            'typo3' => '9.5.0 - 9.9.99',
         ],
         'conflicts' => [
         ],
